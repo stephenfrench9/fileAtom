4ba0dc0e588771d0c3fedace84fdc7d32b4840e4
Stephen French <stephenfrench9@gmail.com>
2018-10-05 15:54:58 -0700

    access internal and external to my app directories

--- app/src/main/java/com/example/stephenfrench/fileatom/MainActivity.java
+++ app/src/main/java/com/example/stephenfrench/fileatom/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.stephenfrench.fileatom;
 
+import android.os.Build;
+import android.os.Environment;
+import android.speech.tts.TextToSpeech;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -19,22 +23,67 @@ public class MainActivity extends AppCompatActivity {
     String mFullAddress;
     String mMessage;
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mTextView = findViewById(R.id.tv);
-        mFullAddress = this.getFilesDir() + "AAA.txt";
-        mMessage = "TutorialsPoint is one the best site in the world\nAnd this is some new info\nand another line";
+        mFullAddress = this.getExternalFilesDir(null) + "/BBB.txt";
+        mMessage = "TutorialsPoint is one the best site in the world\nAnd this is some new info\nand another line\nAlexYerby";
 
         write(mMessage, mFullAddress);
+
+        File caitlin = this.getExternalFilesDir(null);
+        String externalFilesDirAddress = caitlin.getAbsolutePath();
+        Log.d("BASIL", "onCreate(), externalFilesDir is at: " + externalFilesDirAddress);
+        Log.d("BASIL", "onCreate(), externalFilesDir State is: " + String.valueOf(Environment.getExternalStorageState(caitlin)));
+        Log.d("BASIL", "onCreate(), externalFilesDir is emulated: - " + String.valueOf(Environment.isExternalStorageEmulated(caitlin)));
+        Log.d("BASIL", "onCreate(), externalFilesDir is removable: - " + String.valueOf(Environment.isExternalStorageRemovable(caitlin)));
+
+
+        File externalStorageDirectory = Environment.getExternalStorageDirectory();
+        String externalStorageDirectoryAddress = externalStorageDirectory.getAbsolutePath();
+        Log.d("BASIL", "onCreate(), externalStorageDirectory is at: " + externalStorageDirectoryAddress);
+        Log.d("BASIL", "onCreate(), externalStorageDirectory State is: " + Environment.getExternalStorageState(externalStorageDirectory));
+        Log.d("BASIL", "onCreate(), externalStorageDirectory is emulated: - " + String.valueOf(Environment.isExternalStorageEmulated(externalStorageDirectory)));
+        Log.d("BASIL", "onCreate(), externalFilesDir is removable: - " + String.valueOf(Environment.isExternalStorageRemovable(externalStorageDirectory)));
+
+
+        File externalStoragePublicDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        String externalStoragePublicDirectoryAddress = externalStoragePublicDirectory.getAbsolutePath();
+        Log.d("BASIL", "onCreate(), externalStoragePublicDirectory is at: " + externalStoragePublicDirectoryAddress);
+        Log.d("BASIL", "onCreate(), externalStoragePublicDirectory State is: " + Environment.getExternalStorageState(externalStoragePublicDirectory));
+        Log.d("BASIL", "onCreate(), externalStoragePublicDirectory is emulated: - " + String.valueOf(Environment.isExternalStorageEmulated(externalStoragePublicDirectory)));
+        Log.d("BASIL", "onCreate(), externalStoragePublicDirectory is removable: - " + String.valueOf(Environment.isExternalStorageRemovable(externalStoragePublicDirectory)));
+
+        File dataDirectory = Environment.getDataDirectory();
+        String dataDirectoryAddress = dataDirectory.getAbsolutePath();
+        Log.d("BASIL", "onCreate(), dataDirectory is at: " + dataDirectoryAddress);
+//        Log.d("BASIL", "onCreate(), dataDirectory is emulated: " + Environment.isExternalStorageEmulated(dataDirectory));
+
+        File downloadCacheDirectory = Environment.getDownloadCacheDirectory();
+        String downloadCacheDirectoryAddress = downloadCacheDirectory.getAbsolutePath();
+        Log.d("BASIL", "onCreate(), downloadCacheDirectory is at: " + downloadCacheDirectoryAddress);
+//        Log.d("BASIL", "onCreate(), downloadCacheDirectory is emulated: " + Environment.isExternalStorageEmulated(downloadCacheDirectory));
+
+        File rootDirectory = Environment.getRootDirectory();
+        String rootDirectoryAddress = rootDirectory.getAbsolutePath();
+        Log.d("BASIL", "onCreate(), root is at: " + rootDirectoryAddress);
+
+        Log.d("BASIL", "onCreate(), the Overall external storage state is: " + Environment.getExternalStorageState());
+        Log.d("BASIL", "onCreate(), the Overall external storage is emulated: " + Environment.isExternalStorageEmulated());
+        Log.d("BASIL", "onCreate(), the Overall external storage is removable: " + Environment.isExternalStorageRemovable());
+
+
     }
 
     public void zooba(View v) {
         mTextView.setText(read(mFullAddress));
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     private void write(String address, String message) {
         try {
             File file = new File(mFullAddress);
@@ -45,11 +94,12 @@ public class MainActivity extends AppCompatActivity {
             BufferedWriter bw = new BufferedWriter(fw);
             bw.write(mMessage);
             bw.close();
-
             System.out.println("Done");
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+
     }
 
     private String read(String address) {
